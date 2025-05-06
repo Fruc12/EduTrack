93df48759c8ad0d4f40a8d2aee1eb042e6b01ba4
Larissa CHATIGRE <lorychatigre@gmail.com>
2025-05-06 09:37:47 +0100

    ajout du validation

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    username_validator = True
+
+    username = models.CharField(max_length=1, blank=True, null=True)
     ROLES = [('school', 'Etablissement'), ('parent', 'Parent')]
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLES, default='school')
